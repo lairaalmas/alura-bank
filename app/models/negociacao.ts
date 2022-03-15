56bd207196b188364ff349ea9176fdd52c19b168
lairaalmas <lairaalmas@gmail.com>
2022-03-15 09:56:52 -0300

    deixando o codigo mais simples com public readonly

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,39 +2,28 @@ export class Negociacao {
 
 	/*
 	(forma mais verbosa)
-	
-	private _data: Date;
-	private _quantidade: number;
-	private _valor: number;
+
+	private data: Date;
+	private quantidade: number;
+	private valor: number;
 
 	constructor(data: Date, quantidade: number, valor: number) {
-		this._data = data;
-		this._quantidade = quantidade;
-		this._valor = valor;
+		this.data = data;
+		this.quantidade = quantidade;
+		this.valor = valor;
 	}
 	*/
 
 	// se colocar private/public nos parametros do construtor 
 	// o ts já cria esses atributos
 	constructor(
-		private _data: Date,
-		private _quantidade: number,
-		private _valor: number
+		// equivalente a 'private _data: Date' associado a getter 
+		public readonly data: Date,
+		public readonly quantidade: number,
+		public readonly valor: number
 	) { }
 
-	get data(): Date {
-		return this._data;
-	}
-
-	get quantidade(): number {
-		return this._quantidade;
-	}
-
-	get valor(): number {
-		return this._valor;
-	}
-
 	get volume(): number {
-		return this._quantidade * this._valor;
+		return this.quantidade * this.valor;
 	}
 }
