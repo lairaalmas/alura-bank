e4adc28623f3e2d9a1ddf96941d1f64fd2b973fa
lairaalmas <lairaalmas@gmail.com>
2022-03-15 10:18:53 -0300

    tratando casos especificos: metodos de objetos especificos permitindo sobrescrever

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -17,13 +17,30 @@ export class Negociacao {
 	// se colocar private/public nos parametros do construtor 
 	// o ts já cria esses atributos
 	constructor(
-		// equivalente a 'private _data: Date' associado a getter 
-		public readonly data: Date,
+		// 'public readonly data: Date' equivalente a 'private _data: Date' associado a getter
+
+		// IMPORTANTE: o ts lida com muita coisa q garante a regra
+		// de negocio que definimos de "nao deixar sobrescrever", mas um objeto
+		// como o do tipo Date (aqui no exemplo) permite usar o metodos proprios dele
+		// como setDate() e mudar o valor - O ts nao tem como checar todo metodo próprio dele...
+
+		// nesse caso nem essa versao nem a anterior garantem isso
+
+		// SOLUÇÃO: "programação defensiva"
+		// quando for um atributo primitivo deixa assim readonly, mas
+		// quando for objeto, use combo anterior de private+getter porém retorne um clone dele!
+		// Assim, se ele modificarem estarão modificando uma cópia e não o original.
+		private _data: Date,
 		public readonly quantidade: number,
 		public readonly valor: number
 	) { }
 
-	get volume(): number {
+	get volume (): number {
 		return this.quantidade * this.valor;
+	}
+
+	get data (): Date {
+		const cloneData = new Date(this._data.getTime()); // esse novo objeto terá o mesmo valor do outro
+		return cloneData;
 	}
 }
